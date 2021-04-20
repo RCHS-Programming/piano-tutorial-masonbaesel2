d30de3646fbcfbfb3cf2d864c2b31da8095ec726
masonbaesel2 <75792384+masonbaesel2@users.noreply.github.com>
2021-04-20 08:29:12 -0400

    placed the keys on the piano.

--- Piano.java
+++ Piano.java
@@ -14,5 +14,14 @@ public class Piano extends World
     public Piano() 
     {
         super(800, 340, 1);
+        makeKeys();
+    }
+    
+    public void makeKeys()
+    {
+        for(int i=0; i<12; i=i+1)
+        {
+            addObject (new Key ("g","3a.wav"), i*63 + 54, 140);
+        }
     }
 }
